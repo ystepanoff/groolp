a0110faf484251bd4dbfc6eda6781723273978a8
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-29 21:58:37 +0000

    chore: update Plugin interface

--- plugins/plugin_interface.go
+++ plugins/plugin_interface.go
@@ -4,7 +4,10 @@ import "github.com/ystepanoff/groolp/core"
 
 // Plugin defines the interface that all plugins must implement
 type Plugin interface {
+	// RegisterTasks() registers the plugin's tasks with TaskManager
 	RegisterTasks(tm core.TaskManagerInterface) error
-}
 
-const PluginSymbol = "Plugin"
+	GetName() string
+	GetVersion() string
+	GetDescription() string
+}
